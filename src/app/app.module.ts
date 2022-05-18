4d6334fd5e410a776d89823a17d3add68709f40c
Sambhav Jain <samjain8@publicisgroupe.net>
2022-05-18 13:22:08 +0530

    floating banner

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FloatingBannerComponent } from './components/floating-banner/floating-banner.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FloatingBannerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
