e8ecf8787a5c19c7cd5d4e2ee6b70cecf8f8b248
Sambhav Jain <samjain8@publicisgroupe.net>
2022-05-20 13:21:48 +0530

    create div dynamically

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CartViewComponent } from './components/cart-view/cart-view.component';
+import { DynamicallyDivComponent } from './components/dynamically-div/dynamically-div.component';
 import { FloatingBannerComponent } from './components/floating-banner/floating-banner.component';
 import { StudentMarksTableComponent } from './components/student-marks-table/student-marks-table.component';
 import { TimerWithServiceComponent } from './components/timer-with-service/timer-with-service.component';
@@ -12,6 +13,7 @@ const routes: Routes = [
   { path: 'timer', component: TimerComponent },
   { path: 'timerWithService', component: TimerWithServiceComponent },
   { path: 'studentMarks', component: StudentMarksTableComponent },
+  { path: 'dynamicDiv', component: DynamicallyDivComponent },
 ];
 
 @NgModule({
--- src/app/components/dynamically-div/dynamically-div.component.ts
+++ src/app/components/dynamically-div/dynamically-div.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { newArray } from '@angular/compiler/src/util';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dynamically-div',
   templateUrl: './dynamically-div.component.html',
-  styleUrls: ['./dynamically-div.component.css']
+  styleUrls: ['./dynamically-div.component.css'],
 })
 export class DynamicallyDivComponent implements OnInit {
+  myArray = [];
+  currentPosition;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {}
+  @HostListener('window:scroll', ['$event.target'])
+  onScroll(e) {
+    let scroll = window.pageYOffset;
+    if (scroll > this.currentPosition) {
+      this.myArray.length++;
+    } else {
+    }
+    this.currentPosition = scroll;
   }
-
+  ngOnInit(): void {
+    this.currentPosition = window.pageYOffset;
+    this.myArray = new Array(5);
+  }
+  divClicked(value) {
+    alert('div' + ' ' + value + ' is clicked');
+  }
 }
